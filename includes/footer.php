e33dee3f1f68dc9f653b4fbee4f17ab6b557191c
Samuel Gitonga <sammygitongar@gmail.com>
2023-11-16 17:39:51 +0300

    Upload of the front-end part of what I have been working on

---
+++ includes/footer.php
@@ -0,0 +1,12 @@
+
+<div class="footer">
+  <div class="copyright">
+    <p>copright &#169; mashtools 2023. All Rights Reserved</p>
+  </div>
+</div>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,55 @@
+<?php 
+  include('includes/header.php');
+?>
+<div class="totals-container">
+    <div class="totals-header">
+      <h2>CURRENT INVENTORY</h2>
+    </div>
+    <div class="totals-content">
+        <div class="totals-stock">
+            <h2>TOTAL STOCK:&nbsp</h2>
+            <h2>120</h2>
+        </div>
+        <div class="totals-issues">
+            <h2>ISSUED TOOLS:&nbsp</h2>
+              <H2> 48</H2>
+        </div>
+    </div>
+</div>
+<div class="registration-container">
+  <div class="registration-header">
+    <h1 class="display-4 text-center py-4">REGISTRATION</h1>
+  </div>
+  <div class="registration-content py-3">
+    <a class="btn btn-outline-secondary mb-3" href="#">Register a User</a>
+    <a class="btn btn-outline-secondary mb-3" href="#">Register a Tool</a>
+  </div>
+</div>
+
+
+<div class="tools-summary-container">
+  <div class="tools-summary-header">
+    <h2 class="display-3 text-center">Tools Summary</h2>
+  </div>
+  <div class="tools-summary">
+    
+    <div class="tool-card">
+      <div class="tool-header">
+        <img src="assets/images/pliers.png" alt="" class="img-fluid">
+        <div class="layer">
+            <a href="https://karu.ac.ke/" target="_blank"><h3>PLIERS</h3></a>
+        </div>
+      </div>
+      <div class="tool-card-content py-2">
+        <h4 class="tool-name text-sm">In stock:&nbsp <span>75</span></h2>
+        <h4 class="tool-qty text-sm">ISSUED:&nbsp <span>12</span></h2>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+
+<?php
+  include('includes/footer.php');
+?>
