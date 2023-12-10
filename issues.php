df2503e882ec4373a55702ea83d0b885925c0582
Samuel Gitonga <sammygitongar@gmail.com>
2023-12-10 14:31:25 +0300

    Changes kidogo hapa na pale

--- issues.php
+++ issues.php
@@ -1,137 +1,126 @@
 <?php
     include 'includes/header.php';
 ?>
-<body>
-
-    <h1>Tool Issuance Form</h1>
-    <style>
-        .issuance-form{
-            display:flex;
-            flex-direction: column;
-            align-items: center;
-            background: #1c343b17;
-            margin-top: 30px;
-            padding: 0 30px;
-            border-radius: 20px;
-            box-shadow: 2px 3px 5px rgba(0, 0, 0, 0.199);
-            backdrop-filter: blur(10px);
-            width: 40vw;
-            justify-content: space-around;
-            
-        }
-
-      
-
-        .issuehead{
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-        }
-
-        .issuehead img{
-            width: 20vw;
-            border-radius: 20%;
-            margin: 42px 0;
-            border: 3px solid rgb(19, 46, 46);
-            transition: .5s ease;
-        }
-
-        .issuehead img:hover{
-            border-radius: 10px;
-        }
-
-        @media(max-width: 800px){
-            .issuance-form{
-                width: 80vw;
-            }
-
-            .issuehead img{
-                width: 50vw;
-            }
-
-        }
-
-        .issuance-user{
-            display:flex;
-            flex-direction:column;
-            align-items: center;
-        }
-
-        .issuance-tools{
-            display:flex;
-            flex-direction:column;
-            align-items:center;
-        }
 
 
 
+<h1>Tool Issuance Form</h1>
 
-        .issuance-form-container{
-            min-height: 100vh;
-            display:flex;
-            justify-content:center;
 
-        }
-    </style>
-
-  <section class="issuance-form-container">
-        
-      <form id="issuanceForm" class="issuance-form">
-        <div class="issuehead">
-                <img src="assets/images/tools_issue.png" alt="">
-                <h3>Register a new Tool</h3>
-            </div>
-        <!-- User Selection -->
-       <div class="issuance-user">
-        <label for="userSelect">Select User:</label>
-            <div class="issuance-user-contents">
-                <select id="userSelect" name="user">
-                    <option value="user1">User 1</option>
-                    <option value="user2">User 2</option>
-                    <option value="user3">User 3</option>
-                </select>
-                <p class="select-user" id="select-btn" onclick="selectUser()">Select</p>
-            </div>
-       </div>
-
+<section class="issuance-form-container">
     
-
-        <!-- Tools Selection -->
-        <div class="issuance-tools">
-            <label for="toolsSelect">Select Tools</label>
-            <div class="issuance-tools-contents">
-                <select id="toolsSelect" name="tools[]">
-                    <option value="hammer">Hammer</option>
-                    <option value="screwdriver">Screwdriver</option>
-                    <option value="pliers">Pliers</option>
-                    <option value="wrench">Wrench</option>
-                    <option value="tape-measure">Tape Measure</option>
-                    <option value="drill">Drill</option>
-                    <option value="saw">Saw</option>
-                    <option value="level">Level</option>
-                    <option value="safety-goggles">Safety Goggles</option>
-                    <option value="utility-knife">Utility Knife</option>
-                </select>
-                <input type="number" placeholder="Enter Quantity">
-                <button type="button">Add tool</button>
-            </div>
+  <form id="issuanceForm" class="issuance-form">
+        <div class="issuehead">
+            <img src="assets/images/issue.png" alt="">
+            <h3>Register a new Tool</h3>
         </div>
 
+        <div class="form-contents">
+          <div class="user-section">
+            <label for="users">Select a user</label>
+            <select name="users" id="users">
+              <option value="Toza">Toza</option>
+              <option value="Toza">Toza</option>
+              <option value="Toza">Toza</option>
+              <option value="Toza">Toza</option>
+              <option value="Toza">Toza</option>
+            </select>
+          </div>
 
-        <button type="Submit">Submit</button>
-    </form>
+
+          <div class="tools-section">
+             <div class="tool-container">
+                <div class="tool">
+                  <label for="tools" name="tools" id="">Select a tool</label>
+
+                  <select name="tools" id="tools">
+                    <option value="Pliers">Pliers</option>
+                    <option value="Pliers1">Pliers1</option>
+                    <option value="Pliers2">Pliers2</option>
+                    <option value="Pliers3">Pliers3</option>
+                  </select>
+                </div>
+
+                <div class="qty-container">
+                    <div class="qty">
+                      <label for="qty">Qty</label>
+                      <input type="number" name="qty" id="qty">
+                  </div>
+              </div>
+             </div>
+
+             <button id="add-tool" onclick="addTool()">Add</button>
+
+             <div class="selected-tools">
+                <h4>Selected Tools</h4>
+                <!-- <div class="selected-tools-inner">
+                  <input id="selected-tool" type="text">
+                  <input type="number" name="qty" id="selected-qty">
+                </div> -->
+             </div>
+             <button id="submitForm" type="submit" onclick="submitForm()">Submit</button>
+          </div>
+          
+        </div>
+
+</form>
 
 
-  </section>
+</section>
 
 
 
 
 
 
-  <script>
-     
-    </script>
+<script>
+
+    document.getElementById("add-tool").addEventListener('click', function(event){
+    event.preventDefault();
+    });
+
+
+  function addTool(){
+    const tool = document.getElementById("tools");
+    const qty = document.getElementById("qty");
+    const parentDiv = document.querySelector(".selected-tools");
+    let selectedTool;
+
+    // <div class="selected-tools-inner">
+    //               <input id="selected-tool" type="text">
+    //               <input type="number" name="qty" id="selected-qty">
+    //             </div>
+
+    const div = document.createElement("div");
+    div.classList.add("selected-tools-inner");
+
+    const toolInput = document.createElement("input");
+    toolInput.setAttribute("id", "selected-tool");
+    toolInput.setAttribute("type", "text");
+    toolInput.setAttribute("name", "selected-tool");
+
+    toolInput.value = tool.value;
+
+
+    const qtyInput = document.createElement("input");
+    qtyInput.setAttribute("name", "selected-qty");
+    qtyInput.setAttribute("type", "number");
+    qtyInput.setAttribute("id", "selected-qty");
+
+    qtyInput.value = qty.value;
+
+
+    div.appendChild(toolInput);
+    div.appendChild(qtyInput);
+    parentDiv.appendChild(div);  
+  }
+
+  
+
+
+ 
+</script>
+
 
 
 
